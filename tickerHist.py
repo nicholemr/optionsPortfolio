00ee74cf8a68dc42304409b5d3911af72ec4d080
Nichole R <nicholemr@gmail.com>
2020-04-16 15:16:44 -0700

    got tdameritrade module to work and print symbol history data

--- tickerHist.py
+++ tickerHist.py
@@ -1,4 +1,18 @@
 
-consumer_key = WCCVNR4CUNTG8OU7DTHRP82AXAADQNFI
-# TDAUSER
-# TDAPASS
+import tdameritrade as td
+import os
+
+client_id = os.getenv('TDAMERITRADE_CLIENT_ID')
+account_id = os.getenv('TDAMERITRADE_ACCOUNT_ID')
+refresh_token = os.getenv('TDAMERITRADE_REFRESH_TOKEN')
+
+# tdclient = td.TDClient(client_id=client_id, refresh_token=refresh_token, account_ids=[account_id])
+c = td.TDClient()
+
+symbol = 'AAPL'
+periodType = 'day'
+period = 1
+frequencyType = 'minute'
+# frequency = 1
+
+print(c.history(symbol, periodType, period, frequencyType))
